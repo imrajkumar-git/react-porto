1d24e41fefe320ab683d52f988bc3c2fa53bc022
Rajkumar Aryal <aryalrajkumar4@gmail.com>
2022-12-23 09:39:13 +0545

    new file aaded

---
+++ src/components/Messenger/messenger.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+const MessengerChat = () => {
+    return (
+        <>
+            <MessengerCustomerChat
+                pageId="100088818223436"
+                appId="731065684721993"
+            />
+        </>
+    )
+}
+
+export default MessengerChat
